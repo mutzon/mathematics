293c653843f5395a79602ef27d1b11d509851461
Martin Utzon <famutzon@outlook.dk>
2018-08-20 16:41:19 +0200

    Initial Commit

---
+++ mathematics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mathematics
+//
+//  Created by Martin Utzon on 20/08/2018.
+//  Copyright © 2018 Martin Utzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
